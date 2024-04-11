a5f8bb449ae4e461914af6fac59303d356618045
Lolo <loicfinette@gmail.com>
2024-04-11 19:03:57 +0200

    Add sample script to test adb logcat

---
+++ Assets/honeypot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class honeypot : MonoBehaviour
+{
+void Start () {
+
+		Debug.Log( "This is just a simple log lost among others :'-( " );
+
+		Debug.LogWarning( "This is a warning way easier to find :-) " );
+
+		int an_int = 123456;
+		Debug.LogWarning( an_int );
+
+		Debug.LogError( "This is an error ;-) !" );
+
+		int[] array = new int[5];
+		array[5] = 5;   // This will raise an error
+
+	}
+}
